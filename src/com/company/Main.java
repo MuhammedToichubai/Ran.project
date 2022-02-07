febd703e2c3e24d0ebb77efc6dc2db5dfb2b694e
Muhammed <muhammedtoichubai@gmail.com>
2022-02-07 00:32:17 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+import java.util.Scanner;
+import java.util.Random;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+       /* byte thinkpad = 25;
+        int notebook = 1997;
+        System.out.println(thinkpad+notebook);
+        notebook = thinkpad;
+        System.out.println(thinkpad+notebook);*/
+//
+//       System.out.println("\t  * ");
+//       System.out.println("\t *** ");
+//        System.out.println("\t*****");
+//        System.out.println("\t ***");
+//        System.out.println("\t  * ");
+
+//      int a = 0;
+//      int b = 100;
+//      int muha1 = a+ (int) (Math.random()*b);
+//
+//        System.out.println(muha1);
+
+//             Random muha = new Random();
+//              int a = muha.nextInt(10);
+//              int a1 = muha.nextInt(50);
+//              int a2 = muha.nextInt(100);
+//              int summa, arif , result;
+//              summa =a+a1+a2;
+//        System.out.println(a+" + "+a1+" + "+a2+" = "+summa);
+//        arif =(a*a1*a2)/3;
+//        System.out.println( "("+a+" * "+a1+" * "+a2+")/3"+ "="+(double) +arif);
+//        String bir = "Men ";
+//        String eki = "25 ";
+//        String uch = "jashtamyn";
+//        char tort = '!';
+//        System.out.println(bir+eki+uch+tort);
+
+//     Scanner kosh  = new Scanner(System.in);
+//     String konsol = kosh.nextLine();
+//        System.out.println("Salam " +konsol);
+
+    }
+}
